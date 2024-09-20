0d6f5d1085bfd0f0a1e2d1b96702fef398cc692a
Shashank Satish Adsule <shashank.s.adsule@gmail.com>
2024-09-21 02:03:28 +0530

    -- update color in js and css

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-// DARK MODE function
-let bg_flag=true;
-document.getElementById("bg_change").onclick=function()
-{
-    if(bg_flag)
-    {   
-        // dark mode
-        document.documentElement.style.setProperty("--main_bg","#323232"); //rgb(50,50,50)
-        document.documentElement.style.setProperty("--display_bg","#505050"); //rgb(80,80,80)
-        document.documentElement.style.setProperty("--counter_color","#FFFFFF"); //rgb(255,255,255)
-        document.documentElement.style.setProperty("--button_bg","#464646"); //rgb(70,70,70)
-    }
-    else
-    {
-        // light mode
-        document.documentElement.style.setProperty("--main_bg","#FFFFFF"); 
-        document.documentElement.style.setProperty("--display_bg","#FFFFFF");
-        document.documentElement.style.setProperty("--counter_color","#000000"); //rgb(0,0,0)
-        document.documentElement.style.setProperty("--button_bg","#FFFFFF"); 
-    }
-    bg_flag=!bg_flag;
-}
+// color variable
+let bg_color="orange"
+let dark_mode=true;
+let color_map={"orange":["#FFEDCC","#FFEDCC","#FFEDCC"],"blue":["#e1e1f0","#c8c8e1","#c8c8e1"],"green":["#DBEDDB","#A5D2A5","#A5D2A5"],"purple":["#E0C1E0","#CC99CC","#CC99CC"]};
 
 // COLOR MODE function
 document.getElementById("color_options").addEventListener("change",function()
 {
-    let color=this.value;
+    bg_color=this.value;
     let image_element=document.querySelectorAll("#user_choice img");
     
-    switch(color)
+    switch(bg_color)
     {
         case "orange":
             document.documentElement.style.setProperty("--header_bg","#ff6400");                                                //for header
@@ -35,6 +17,7 @@ document.getElementById("color_options").addEventListener("change",function()
                 x.style.filter="invert(58%) sepia(16%) saturate(5312%) hue-rotate(342deg) brightness(104%) contrast(101%)";     //for option buttons
             });
             document.documentElement.style.setProperty("--text_color","#ffa500");                                               //for text
+            
             break;
         case "blue":
             document.documentElement.style.setProperty("--header_bg","#3838E4");            //better
@@ -60,6 +43,27 @@ document.getElementById("color_options").addEventListener("change",function()
     }
 });
 
+// DARK MODE function
+document.getElementById("bg_change").onclick=function()
+{
+    if(dark_mode)
+    {   
+        // dark mode
+        document.documentElement.style.setProperty("--main_bg","#323232"); //rgb(50,50,50)
+        document.documentElement.style.setProperty("--display_bg","#505050"); //rgb(80,80,80)
+        document.documentElement.style.setProperty("--counter_color","#FFFFFF"); //rgb(255,255,255)
+        document.documentElement.style.setProperty("--button_bg","#464646"); //rgb(70,70,70)
+    }
+    else
+    {
+        // light mode
+        document.documentElement.style.setProperty("--main_bg",color_map[bg_color][0]); //rgb(225,225,240)
+        document.documentElement.style.setProperty("--display_bg",color_map[bg_color][1]); //rgb(200,200,225)
+        document.documentElement.style.setProperty("--counter_color","#000000"); //rgb(0,0,0)
+        document.documentElement.style.setProperty("--button_bg",color_map[bg_color][2]); 
+    }
+    dark_mode=!dark_mode;
+}
 
 // ROCK_PAPER_SCICCSOR button functions
 let user_cnt=0, computer_cnt=0;                                 // 0->rock ,1->paper, 2->scissor
